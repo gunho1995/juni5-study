fd2f06845ae0a5e7001f9cdb52cbb3223df93a03
Crispy <gunho19945@gmail.com>
2021-12-19 23:42:42 +0900

    [개발] 테스트 코드 작성

---
+++ src/main/java/lotto/ParkingSystem.java
@@ -0,0 +1,10 @@
+package lotto;
+
+public class ParkingSystem {
+    public static void main(String[] args) {
+        int lottoNumbers[] = {1, 2, 3, 4, 5, 100};
+
+        for(int index : )
+
+    }
+}
